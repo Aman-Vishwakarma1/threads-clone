5b80eb9bdbbfe98dd2e263daf490af96ca0638ff
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-01-29 17:37:55 +0530

    remove build dir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
 import customError from "./utils/error";
 import errorHandler from "./middleware/errorHandler";
 
@@ -26,7 +27,7 @@ async function startServer() {
   });
   await graphqlServer.start();
 
-  app.use(express.json());
+  app.use(cors(), express.json());
 
   app.get("/", (req, res, next) => {
     res.status(200).json({ message: "Server is Up", status: "live" });
@@ -34,9 +35,10 @@ async function startServer() {
 
   app.use("/graphql", expressMiddleware(graphqlServer) as express.Express);
 
+  app.use(errorHandler);
+
   app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
   });
 }
-
 startServer();
